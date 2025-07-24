0b8d570f8156f053a533c58e3a8525c3794190ed
jinkachandu9 <jinkachandu9@gmail.com>
2025-07-24 22:51:39 +0530

    release: update exception

--- src/main/java/com/autofiller/exception/GlobalExceptionHandler.java
+++ src/main/java/com/autofiller/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException.NotFound;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 @ControllerAdvice
@@ -13,7 +14,7 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleNotFound(WebClientResponseException.NotFound ex) {
         ErrorResponse error = new ErrorResponse(
             HttpStatus.NOT_FOUND.value(),
-            "Resource not found: " + ex.getMessage()
+            "Resource not found from downstream services" 
         );
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
@@ -25,5 +26,14 @@ public class GlobalExceptionHandler {
             ex.getMessage()
         );
         return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(NotFound.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFound(NotFound ex) {
+        ErrorResponse error = new ErrorResponse(
+            HttpStatus.NOT_FOUND.value(),
+            ex.getMessage()
+        );
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
